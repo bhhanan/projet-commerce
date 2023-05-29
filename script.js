97637690ba79be49a16dae6f24238c282b504722
hananebahoum7@gmail.com <Vous@exemple.com>
2023-05-29 12:42:42 +0100

    design

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ if(close) {
         nav.classList.remove('active');
     })
 }
+
+
+
+
+
+document.querySelectorAll('.pro-container img').forEach(image =>{
+  image.onclick = () =>{
+    document.querySelector('.popup-imag').style.display = 'block';
+    document.querySelector('.popup-imag img').src = image.getAttribute('src');
+  }
+});
+
+document.querySelector('.popup-imag span').onclick = () =>{
+    document.querySelector('.popup-imag').style.display = 'none';
+}
